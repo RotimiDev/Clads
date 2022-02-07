745459d2bd61ba17c08c733b9cbcc58b6d40bb24
weeveedenny <dennisodalonu@gmail.com>
2022-02-07 18:14:03 +0100

    implemented nav_bar tool_bar nav_drawer

---
+++ app/src/main/java/com/decagonhq/clads_client/presentation/ui/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.decagonhq.clads_client.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.decagonhq.clads_client.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
