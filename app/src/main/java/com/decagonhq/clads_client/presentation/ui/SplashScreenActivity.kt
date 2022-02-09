0c326f4d55401b962281951e7edcfd14961e945d
Decagon <mac@macs-MacBook-Air.local>
2022-02-09 16:52:20 +0100

    First commit

---
+++ app/src/main/java/com/decagonhq/clads_client/presentation/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.decagonhq.clads_client.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.decagonhq.clads_client.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
