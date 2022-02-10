8ff4e1d498d7f3948f44c4138a1d69bee4c53bef
weeveedenny <dennisodalonu@gmail.com>
2022-02-10 19:18:18 +0100

    additional tests

--- app/src/main/java/com/decagonhq/clads_client/presentation/ui/AuthenticationActivity.kt
+++ app/src/main/java/com/decagonhq/clads_client/presentation/ui/AuthenticationActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.decagonhq.clads_client.R
 
+
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
     }
+
+
 }
