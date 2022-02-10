0e62060ab5770c7231058e158318bac2ecec67a7
Rotimi Akeem <akeemrotimi10@gmail.com>
2022-02-10 20:29:47 +0100

    added ui testing

---
+++ app/src/main/java/com/decagonhq/clads_client/presentation/ui/SignupActivity.kt
@@ -0,0 +1,14 @@
+package com.decagonhq.clads_client.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.decagonhq.clads_client.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+    }
+}
