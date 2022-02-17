e11ea67118ab655695bbaaa322daea9e5cf6788d
Decagon <decagon@Decagons-MacBook-Air-5.local>
2022-02-17 08:56:36 +0100

    Run Ktlint

--- app/src/main/java/com/decagonhq/clads_client/presentation/ui/HomeFragment.kt
+++ app/src/main/java/com/decagonhq/clads_client/presentation/ui/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.decagonhq.clads_client.R
 
 class HomeFragment : Fragment() {
@@ -19,5 +17,4 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 }
