9bb3ef9bad195d9207b5bcc9474991f1200d941c
Dubem Ezeagwu <48288857+dubemezeagwu@users.noreply.github.com>
2022-02-23 11:11:29 +0100

    Code clean up.

--- app/src/main/java/com/decagonhq/clads_client/presentation/di/CladsAppModule.kt
+++ app/src/main/java/com/decagonhq/clads_client/presentation/di/CladsAppModule.kt
@@ -2,7 +2,6 @@ package com.decagonhq.clads_client.presentation.di
 
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import dagger.hilt.components.SingletonComponent
 
 @Module
