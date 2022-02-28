c509e2c10fe6107d1480ceaff4d16e51011c0fb4
weeveedenny <dennisodalonu@gmail.com>
2022-02-28 15:44:47 +0100

    resolved conflicts

--- app/src/main/java/com/decagonhq/clads_client/presentation/utils/Resource.kt
+++ app/src/main/java/com/decagonhq/clads_client/presentation/utils/Resource.kt
@@ -4,6 +4,4 @@ sealed class Resource<T> (var data: T? = null, var message: String? = null) {
     class Loading<T>() : Resource<T>()
     class Success<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(data: T?, message: String?) : Resource<T>(null, message)
-
-
 }
