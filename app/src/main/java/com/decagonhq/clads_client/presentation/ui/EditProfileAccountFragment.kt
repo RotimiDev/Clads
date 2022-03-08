a45ec176cc86c76d27d6e7d1c20477072c9db2d6
Decagon <decagon@Decagons-MacBook-Air-5.local>
2022-03-08 17:38:58 +0100

    before conflict

--- app/src/main/java/com/decagonhq/clads_client/presentation/ui/EditProfileAccountFragment.kt
+++ app/src/main/java/com/decagonhq/clads_client/presentation/ui/EditProfileAccountFragment.kt
@@ -1,13 +1,11 @@
 package com.decagonhq.clads_client.presentation.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.decagonhq.clads_client.R
