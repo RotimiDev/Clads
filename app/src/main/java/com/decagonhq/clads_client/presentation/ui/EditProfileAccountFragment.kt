597c009002cabf6f58be60494fce8fe9c6efeeb8
Rotimi Akeem <akeemrotimi10@gmail.com>
2022-03-14 12:08:43 +0100

    View model package name refactored

--- app/src/main/java/com/decagonhq/clads_client/presentation/ui/EditProfileAccountFragment.kt
+++ app/src/main/java/com/decagonhq/clads_client/presentation/ui/EditProfileAccountFragment.kt
@@ -22,9 +22,9 @@ import com.decagonhq.clads_client.R
 import com.decagonhq.clads_client.databinding.FragmentEditProfileAccountBinding
 import com.decagonhq.clads_client.presentation.model.DeliveryAddresse
 import com.decagonhq.clads_client.presentation.model.UpdateProfileRequest
+import com.decagonhq.clads_client.presentation.viewmodel.DashboardViewModel
 import com.decagonhq.clads_client.presentation.viewmodel.EditProfileViewModel
 import com.decagonhq.clads_client.presentation.viewmodel.UpdateProfileViewModel
-import com.decagonhq.clads_client.presentation.viewmodel.DashboardViewModel
 import com.decagonhq.clads_client.utils.Resource
 import com.decagonhq.clads_client.utils.SessionManager
 import com.decagonhq.clads_client.utils.SessionManager.TOKEN
